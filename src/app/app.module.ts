06d9b6bf43254a7e4401980edac06e2cbc512894
nlabandera <nlabandera@countercraftsec.com>
2022-05-20 12:40:09 +0200

    insects component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { InsectsComponent } from './insects/insects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InsectsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/insects/insects.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InsectsComponent } from './insects.component';
+
+describe('InsectsComponent', () => {
+  let component: InsectsComponent;
+  let fixture: ComponentFixture<InsectsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InsectsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InsectsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
