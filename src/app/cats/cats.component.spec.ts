87dc220ea81ed88e4b3ec8842856552cb4a0be53
nlabandera <nlabandera@countercraftsec.com>
2022-06-08 17:20:54 +0200

    first test setup

--- src/app/cats/cats.component.spec.ts
+++ src/app/cats/cats.component.spec.ts
@@ -1,16 +1,21 @@
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSort } from '@angular/material/sort';
+import { CatService } from './cat.service';
 
 import { CatsComponent } from './cats.component';
 
-describe('CatsComponent', () => {
+fdescribe('CatsComponent', () => {
   let component: CatsComponent;
   let fixture: ComponentFixture<CatsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CatsComponent ]
+      imports: [ ],
+      declarations: [CatsComponent, MatSort],
+      providers: [CatService, HttpClient, HttpHandler],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
